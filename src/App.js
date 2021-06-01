f2f0977ec4711b57eff8c8bd20531aab20981841
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-06-01 20:49:32 +0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import HomePage from "./pages/HomePage";
 import PopularPage from "./pages/PopularPage";
+import {Route, Switch} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <HomePage />;
+    return (
+        <div>
+            <NavBar/>
+            <Switch>
+                <Route path='/popular'>
+                    <PopularPage/>
+                </Route>
+                <Route path='/'>
+                    <HomePage/>;
+                </Route>
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Col, Container, Nav, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+
+
+function NavBar() {
+    return (
+       <Container>
+           <Row>
+               <Col>
+                   <Nav variant="pills" as='ul'>
+                       <Nav.Item as='li'>
+                           <Nav.Link as={NavLink} activeClassName="active" to="/home">Home</Nav.Link>
+                       </Nav.Item>
+                       <Nav.Item as='li'>
+                           <Nav.Link as={NavLink} activeClassName="active" to="/popular">Popular</Nav.Link>
+                       </Nav.Item>
+                   </Nav>
+               </Col>
+           </Row>
+       </Container>
+    )
+}
+
+export default NavBar;
