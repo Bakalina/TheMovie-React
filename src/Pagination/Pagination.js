0230291f730f1e24a3f66969434b274eeb9f6a51
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-06-04 11:45:09 +0300

    Pagination-1.2

--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -2,23 +2,41 @@ import React from 'react'
 import {Pagination} from 'react-bootstrap';
 
 
+class PaginationPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageNumber: 1
+        }
 
-const PaginationPage = () => {
+    }
 
-const active = 1 ;
-const items = [];
-for (let number = 1; number <= 5; number++) {
-    items.push(
-        <Pagination.Item key={number} active={number === active && active}>
-            {number}
-        </Pagination.Item>,
-    );
 
+    paginationFetch = () => {
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${this.state.pageNumber}`)
+            .then(res => res.json())
+            .then(response => this.setState({
+                movies: response.results
+            }));
+    }
+
+    render() {
+        const active = this.state.pageNumber;
+        const items = [];
+        for (let number = 1; number <= 5; number++) {
+            items.push(
+                <Pagination.Item key={number} active={number === active && active}>
+                    {number}
+                </Pagination.Item>,
+            );
+
+        }
+        return (
+            <Pagination>{items}</Pagination>
+        )
+
+    }
 }
 
-    return (
-        <Pagination>{items}</Pagination>
-    )
-};
 
 export default PaginationPage;
--- src/pages/PopularPage.js
+++ src/pages/PopularPage.js
@@ -1,36 +1,39 @@
 import React, {Component} from 'react'
 import {Container, Row, Col} from 'react-bootstrap';
 import {Movies} from '../components/Movies';
-import PaginationPage from "../Pagination/Pagination";
 
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            movies: []
+            movies: [],
+            valBtn: 1
         };
     }
 
 
+    apiFetchMovies = () => {
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${this.state.valBtn}`)
+            .then(res => res.json())
+            .then(response => this.setState({
+                movies: response.results
+            }));
+    }
+
+
     componentDidMount() {
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca`)
-            .then(res => res.json())
-            .then(response => this.setState({
-                movies: response.results
-            }));
+        this.apiFetchMovies()
     }
 
-    btn = () => {
-        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca`)
-            .then(res => res.json())
-            .then(response => this.setState({
-                movies: response.results
-            }));
 
-
+    componentDidUpdate() {
+        this.apiFetchMovies()
     }
 
+    clickPage = (e) => {
+        this.setState({valBtn: e.target.value})
+    }
 
 
 
@@ -39,11 +42,17 @@ export default class HomePage extends Component {
             <Container>
                 <Row>
                     <Col>
-                        <PaginationPage/>
+                        <button value='1' onClick={e => this.clickPage(e)}>1</button>
+                        <button value='2' onClick={e => this.clickPage(e)}>2</button>
+                        <button value='3' onClick={e => this.clickPage(e)}>3</button>
+                        <button value='4' onClick={e => this.clickPage(e)}>4</button>
+                        <button value='5' onClick={e => this.clickPage(e)}>5</button>
                         <Movies data={this.state.movies}/>
                     </Col>
                 </Row>
             </Container>
         )
     }
+
+
 }
