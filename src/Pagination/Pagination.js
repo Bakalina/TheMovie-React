33ff9759cf13214e099c605cd6bc64e0122d236f
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-07-05 00:32:38 +0300

    Pagination-1.6

--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -1,40 +1,45 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Pagination} from 'react-bootstrap';
 
 
-const PaginationPage = function ({activePage}, {totalPages}) {
+export default class PaginationPage extends Component {
+    constructor(props) {
+        super(props);
+    }
 
+    handlePageChange(page) {
+        this.getAllMovies(page);
+    }
 
-    return (
-        <Pagination>
-            <Pagination.First onClick={() => this.handlePageChange(1)}/>
-            <Pagination.Prev onClick={() => this.handlePageChange(activePage - 1)} disabled={activePage === 1}/>
-            {Array(9).fill(0).map((item, index) => {
-                if (activePage && (activePage > totalPages - 8)) {
+    render() {
+        const {activePage, totalPages} = this.state;
+        return (
+            <Pagination className="justify-content-center">
+                <Pagination.First onClick={() => this.handlePageChange(1)}/>
+                <Pagination.Prev onClick={() => this.handlePageChange(activePage - 1)} disabled={activePage === 1}/>
+                {Array(9).fill(0).map((item, index) => {
+                    if (activePage && (activePage > totalPages - 8)) {
+                        return (
+                            <Pagination.Item
+                                onClick={() => this.handlePageChange(totalPages - 8 + index)}
+                                active={activePage === totalPages - 8 + index}
+                            >
+                                {totalPages - 8 + index}
+                            </Pagination.Item>
+                        );
+                    }
                     return (
-                        <Pagination.Item
-                            onClick={() => this.handlePageChange(totalPages - 8 + index)}
-                            active={activePage === totalPages - 8 + index}
-                        >
-                            {totalPages - 8 + index}
+                        <Pagination.Item onClick={() => this.handlePageChange(activePage + index)}
+                                         active={activePage === activePage + index}>
+                            {activePage + index}
                         </Pagination.Item>
                     );
-                }
-                return (
-                    <Pagination.Item onClick={() => this.handlePageChange(activePage + index)}
-                                     active={activePage === activePage + index}>
-                        {activePage + index}
-                    </Pagination.Item>
-                );
-            })}
-            <Pagination.Next onClick={() => this.handlePageChange(activePage + 1)}
-                             disabled={activePage === {totalPages}}/>
-            <Pagination.Last onClick={() => this.handlePageChange({totalPages})}/>
-        </Pagination>
-    )
-
-
+                })}
+                <Pagination.Next onClick={() => this.handlePageChange(activePage + 1)}
+                                 disabled={activePage === totalPages}/>
+                <Pagination.Last onClick={() => this.handlePageChange(totalPages)}/>
+            </Pagination>
+        )
+    }
 }
 
-
-export default PaginationPage;
--- src/pages/PopularPage.js
+++ src/pages/PopularPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Pagination} from 'react-bootstrap';
 import {Movies} from '../components/Movies';
 import PaginationPage from "../Pagination/Pagination";
 
@@ -13,6 +13,7 @@ export default class HomePage extends Component {
             totalPages: 0
         };
     }
+
 
     getAllMovies(page) {
         fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${page}`)
@@ -28,13 +29,39 @@ export default class HomePage extends Component {
         this.getAllMovies(1);
     }
 
+    handlePageChange(page) {
+        this.getAllMovies(page);
+    }
 
 
 
     render() {
+        const { activePage, totalPages } = this.state;
         return (
             <Container>
-                <PaginationPage activePage={this.state.activePage} totalPages={this.state.totalPages}/>
+                <Pagination className="justify-content-center">
+                    <Pagination.First onClick={() => this.handlePageChange(1)} />
+                    <Pagination.Prev onClick={() => this.handlePageChange(activePage - 1)} disabled={activePage === 1} />
+                    {Array(9).fill(0).map((item, index) => {
+                        if (activePage && (activePage > totalPages - 8)) {
+                            return (
+                                <Pagination.Item
+                                    onClick={() => this.handlePageChange(totalPages - 8 + index)}
+                                    active={activePage === totalPages - 8 + index}
+                                >
+                                    {totalPages - 8 + index}
+                                </Pagination.Item>
+                            );
+                        }
+                        return (
+                            <Pagination.Item onClick={() => this.handlePageChange(activePage + index)} active={activePage === activePage + index}>
+                                {activePage + index}
+                            </Pagination.Item>
+                        );
+                    })}
+                    <Pagination.Next onClick={() => this.handlePageChange(activePage + 1)} disabled={activePage === totalPages} />
+                    <Pagination.Last onClick={() => this.handlePageChange(totalPages)} />
+                </Pagination>
                 <Row>
                     <Col>
                         <Movies data={this.state.movies}/>
