6bc02a85a0f223f9fc3d74d1ea6486ada092a021
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-08-27 23:46:23 +0300

    style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,36 @@
 import React from "react";
 import {Col, Container, Nav, Row} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
+import styles from './NavBar.module.css'
+
+
 
 
 function NavBar() {
     return (
-       <Container>
-           <Row>
-               <Col>
-                   <Nav variant="pills" as='ul'>
-                       <Nav.Item as='li'>
-                           <Nav.Link as={NavLink} activeClassName="active" to="/home">Home</Nav.Link>
-                       </Nav.Item>
-                       <Nav.Item as='li'>
-                           <Nav.Link as={NavLink} activeClassName="active" to="/popular">Popular</Nav.Link>
-                       </Nav.Item>
-                       <Nav.Item as='li'>
-                           <Nav.Link as={NavLink} activeClassName="active" to="/expected">Expected</Nav.Link>
-                       </Nav.Item>
-                       <Nav.Item as='li'>
-                           <Nav.Link as={NavLink} activeClassName="active" to="/now_playing">NowPlaying</Nav.Link>
-                       </Nav.Item>
-                   </Nav>
-               </Col>
-           </Row>
-       </Container>
+        <header className={styles.nav}>
+            <Container >
+                <Row>
+                    <Col>
+                        <Nav variant="pills" as='ul' >
+                            <Nav.Item as='li'>
+                                <Nav.Link as={NavLink} activeClassName="active" to="/home">Home</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link as={NavLink} activeClassName="active" to="/popular">Popular</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link as={NavLink} activeClassName="active" to="/expected">Expected</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item as='li'>
+                                <Nav.Link as={NavLink} activeClassName="active" to="/now_playing">NowPlaying</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
+        </header>
+
     )
 }
 
