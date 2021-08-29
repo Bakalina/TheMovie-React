b6387bd78f1fadec07ea67e2138365b1c01ff074
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-08-30 00:04:45 +0300

    component pages were moved to functional

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 import {Container} from 'react-bootstrap';
 import {Movies} from '../components/Movies';
 import Pagination from "../components/Pagination";
@@ -9,8 +9,7 @@ export default function HomePage() {
     const [totalPages, setTotalPages] = useState(0);
 
     function getMovies(page = '') {
-
-        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${page}`)
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${page}`)
             .then(res => res.json())
             .then(({results, total_pages}) => {
                 setMovies(results);
--- src/pages/PopularPage.js
+++ src/pages/PopularPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 import {Container} from 'react-bootstrap';
 import {Movies} from '../components/Movies';
 import Pagination from "../components/Pagination";
@@ -7,10 +7,8 @@ import Pagination from "../components/Pagination";
 export default function PopularPage() {
     const [movies, setMovies] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
-    console.log(totalPages)
 
     function getMovies(page = '') {
-        console.log(page)
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${page}`)
             .then(res => res.json())
             .then(({results, total_pages}) => {
@@ -26,67 +24,4 @@ export default function PopularPage() {
         </Container>
     )
 
-
 }
-
-
-// export default class PopularPage extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             movies: [],
-//             totalPages: 0,
-//             isModalOpened: false
-//         };
-//     }
-//
-//
-//     getMovies(page='') {
-//
-//         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=b3eddc3e1c353736590c8f4251c8afca&page=${page}`)
-//             .then(res => res.json())
-//             .then(({ results, page, total_pages }) => this.setState({
-//                 movies: results,
-//                 activePage: page,
-//                 totalPages: total_pages
-//             }));
-//     }
-//
-//     handlePageChange = ({ selected }) => {
-//         this.getMovies(selected + 1);
-//     }
-//
-//
-//     render() {
-//         return (
-//             <Container>
-//                 <Row>
-//                     <Col>
-//                         <Movies data={this.state.movies}/>
-//                     </Col>
-//                 </Row>
-//                 <ReactPaginate
-//                     pageCount={this.state.totalPages}
-//                     pageRangeDisplayed={5}
-//                     marginPagesDisplayed={2}
-//                     onPageChange={this.handlePageChange}
-//                     containerClassName="pagination justify-content-center"
-//                     pageClassName="page-item"
-//                     pageLinkClassName="page-link"
-//                     breakClassName="page-item"
-//                     breakLinkClassName="page-link"
-//                     nextLabel="›"
-//                     previousLabel="‹"
-//                     nextClassName="page-item"
-//                     nextLinkClassName="page-link"
-//                     previousClassName="page-item"
-//                     previousLinkClassName="page-link"
-//                     activeClassName="active"
-//                     initialPage={0}
-//                 />
-//             </Container>
-//         )
-//     }
-//
-//
-// }
