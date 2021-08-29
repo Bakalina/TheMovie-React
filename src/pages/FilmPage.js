d4d80095bf1bd1b83db4b795611a807f8e375466
bakalina.yaroslav@gmail.com <bakalina.yaroslav@gmail.com>
2021-08-30 00:56:36 +0300

    component FilmPage were moved to functional

--- src/pages/FilmPage.js
+++ src/pages/FilmPage.js
@@ -1,41 +1,33 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import {Container, Row, Col} from "react-bootstrap";
 
 
-class FilmPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            movie: {}
-        }
-    }
+export default function FilmPage (props) {
+    const [movie, setMovie] = useState({})
 
+    useEffect(() => {
+            fetch(`https://api.themoviedb.org/3/movie/${props.match.params.id}?api_key=b3eddc3e1c353736590c8f4251c8afca`)
+                .then(res => res.json())
+                .then(response => setMovie(response))
+        },[])
 
-    componentDidMount() {
-        fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=b3eddc3e1c353736590c8f4251c8afca`)
-            .then(res => res.json())
-            .then(response => this.setState({movie: response}))
-    }
+    const {poster_path, title, overview, runtime, release_date} = movie
 
-    render() {
-        const {poster_path, title, overview, runtime, release_date} = this.state.movie
-        return (
-            <Container>
-                <Row>
-                    <Col sm={4}>
-                        <img style={{maxWidth: '100%'}}
-                             src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${poster_path}`}/>
-                    </Col>
-                    <Col sm={8}>
-                        <h2>{title}</h2>
-                        <p><b>Release: </b>{release_date}</p>
-                        <p><b>Overview: </b>{overview}</p>
-                        <p><b>Runtime: </b>{runtime} min</p>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+    return (
+        <Container>
+            <Row>
+                <Col sm={4}>
+                    <img alt='image' style={{maxWidth: '100%'}}
+                         src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${poster_path}`}/>
+                </Col>
+                <Col sm={8}>
+                    <h2>{title}</h2>
+                    <p><b>Release: </b>{release_date}</p>
+                    <p><b>Overview: </b>{overview}</p>
+                    <p><b>Runtime: </b>{runtime} min</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+
 }
-
-export default FilmPage;
